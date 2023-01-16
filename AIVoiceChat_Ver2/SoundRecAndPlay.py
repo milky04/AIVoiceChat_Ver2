b9fa7cb0612be1218381cea6031e306970c79e20
milky04 <76756065+milky04@users.noreply.github.com>
2023-01-17 01:40:37 +0900

    Add files via upload

---
+++ AIVoiceChat_Ver2/SoundRecAndPlay.py
@@ -0,0 +1,52 @@
+import pyaudio
+import wave
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+# wav形式で録音
+def recsound(input_wav, record_time):
+    CHUNK = 2**10
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK
+                    )
+    
+    print(str(record_time) + "秒間録音します。何か話しかけてください。")
+    frames = []
+    for i in range(0, int(RATE / CHUNK * record_time)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("録音が完了しました。")
+    
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    
+    wf = wave.open(input_wav, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+# wavの音声を再生(path指定ではない(pathの指定でもいける？))
+def playsound(output_wav):
+    WIDTH = 2
+    CHANNELS = 1
+    RATE = 44100
+
+    sound = AudioSegment(output_wav,
+                        sample_width=WIDTH, 
+                        channels=CHANNELS, 
+                        frame_rate=RATE
+                        )
+    play(sound)
