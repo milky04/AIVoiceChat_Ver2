0f9ba83c07f6af385e2442a31e6ebed499ab07d0
milky04 <76756065+milky04@users.noreply.github.com>
2023-01-17 01:08:50 +0900

    Update LaunchApplication.py

--- LaunchApplication.py
+++ LaunchApplication.py
@@ -3,3 +3,6 @@ from subprocess import PIPE
 
 def launch(application_path):
     subprocess.Popen(application_path, shell=True, stdout=subprocess.PIPE)
+
+application = r"COEIROINKのパスを記載"
+launch(application)
