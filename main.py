ef22c531774a3d57bf961c3776a48ca82fa8f4cc
milky04 <76756065+milky04@users.noreply.github.com>
2023-01-18 01:26:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-import LaunchApplication
+# import LaunchApplication
 import SoundRecAndPlay as sound
 import ConvertVoiceToText
 import GenerateText
 import SpeechSynthesis
 
-# COEIROINKのパスを指定して起動
-application = r"COEIROINKのパスを記載"
-LaunchApplication.launch(application)
+# # COEIROINKのパスを指定して起動
+# application = r"C:\Users\youjo\OneDrive\デスクトップ\アプリ\COEIROINK-GPU-v.1.6.0\COEIROINKonVOICEVOX.exe"
+# LaunchApplication.launch(application)
 
 # 録音時間を指定
 record_time = 5
-# 録音データのパスを指定
-input_path = ".\wav\input.wav"
 
 print("処理を開始します。終了する際はターミナルに「Ctrl+C」を入力してください。")
 try:
     while True:
         # 録音データのパスと録音時間を指定して録音
-        sound.recsound(input_path, record_time)
-        # input_pathに保存された録音データをConvertVoiceToTextでテキストに変換
-        input_text = ConvertVoiceToText.convert_voice_to_text(input_path)
+        input_sound = sound.recsound(record_time)
+        # input_soundに保存された録音データをConvertVoiceToTextでテキストに変換
+        input_text = ConvertVoiceToText.convert_voice_to_text(input_sound)
         # input_textをGenerateTextに投げて生成された文章を受け取る
         result_text = GenerateText.generate_text(input_text)
         print("AI:" + result_text)
