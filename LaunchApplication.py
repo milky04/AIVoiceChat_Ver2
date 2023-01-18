e52ecdbb030abf7dff5c1023a61c0cdb3a9f958f
milky04 <76756065+milky04@users.noreply.github.com>
2023-01-19 01:50:17 +0900

    Update LaunchApplication.py

--- LaunchApplication.py
+++ LaunchApplication.py
@@ -1,84 +1,34 @@
 import subprocess
 from subprocess import PIPE
 import sys
+import requests
 
-# def launch(application_path, request):
-#     # アプリケーションをバックグラウンドで起動
-#     pro = subprocess.Popen(application_path, stdout=subprocess.PIPE)
+def launch(application_path, url):
+    # アプリケーションをバックグラウンドで起動
+    process = subprocess.Popen(application_path, stdout=subprocess.PIPE)
 
-#     # カウンタ初期値設定
-#     cnt = 1
-#     # カウンタ上限設定
-#     cnt_limit = 10
-#     print("アプリケーションにリクエストを送信し、起動確認をします。")
-#     print("アプリケーションに" + str(cnt) + "回目のリクエストを送信します。")
-#     # アプリケーションが起動するまでリクエストを送信し続けて起動確認
-#     while True:
-#         try:
-#             print("アプリケーションの起動確認中………")
-#             request
-#             print("起動しました")
-#             break
-#         except:
-#             cnt += 1
-#             if cnt > cnt_limit:
-#                 print("リクエストの送信回数が" + str(cnt_limit) + "回を超えました。処理の実行を終了します。")
-#                 pro.kill() # プロセスを終了
-#                 sys.exit()
-#             else:
-#                 print("アプリケーションに" + str(cnt) + "回目のリクエストを送信します。")
-#                 continue
-#         # finally:
-#         #     print("done")
-
-#     print("done")
-
-
-# import SpeechSynthesis
-# import SoundRecAndPlay as sound
-
-# application = r"COEIROINKのパスを記載"
-# request = SpeechSynthesis.speech_synthesis("起動しました")
-# launch(application, request)
-
-
-
-
-
-def launch(application_path):
-    pro = subprocess.Popen(application_path, stdout=subprocess.PIPE)
-    return pro
-
-application = r"COEIROINKのパスを記載"
-pro = launch(application)
-
-
-import SpeechSynthesis
-import SoundRecAndPlay as sound
-
-
-cnt = 1
-cnt_limit = 10
-print("アプリケーションにリクエストを送信し、起動確認をします。")
-print("アプリケーションに" + str(cnt) + "回目のリクエストを送信します。")
-
-while True:
-    try:
-        print("アプリケーションの起動確認中………")
-        res = SpeechSynthesis.speech_synthesis("起動しました")
-        sound.playsound(res)
-        print("起動しました")
-        break
-    except:
-        cnt += 1
-        if cnt > cnt_limit:
-            print("リクエストの送信回数が" + str(cnt_limit) + "回を超えました。処理の実行を終了します。")
-            pro.kill() # プロセスを終了
-            sys.exit()
-        else:
+    # カウンタ初期値設定
+    cnt = 1
+    # カウンタ上限設定
+    cnt_limit = 10
+    print("アプリケーションにリクエストを送信し、起動確認をします。")
+    # アプリケーションが起動するまでリクエストを送信し続けて起動確認
+    while True:
+        try:
             print("アプリケーションに" + str(cnt) + "回目のリクエストを送信します。")
-            continue
-    # finally:
-    #     print("done")
-
-print("done")
+            print("アプリケーションの起動確認中………")
+            # POSTで指定URLにリクエストを送信して起動確認
+            requests.post(url)
+            print("起動しました")
+            break
+        except:
+            cnt += 1
+            if cnt > cnt_limit:
+                print("リクエストの送信回数が" + str(cnt_limit) + "回を超えました。処理の実行を終了します。")
+                # プロセスを終了
+                process.kill()
+                sys.exit()
+            else:
+                continue
+            
+    return process
