ef353903a0445e42e18eb5a1a2ca4a9c3a8c4d8c
milky04 <76756065+milky04@users.noreply.github.com>
2023-02-28 02:32:33 +0900

    Update ConvertVoiceToText.py

--- ConvertVoiceToText.py
+++ ConvertVoiceToText.py
@@ -1,5 +1,4 @@
 import whisper
-import os
 
 whisper_model = whisper.load_model("small")
 language = 'ja'
@@ -12,5 +11,4 @@ def convert_voice_to_text(audio):
         fp16=False
         )
     result_text = result['text']
-    os.remove(audio)
     return result_text
